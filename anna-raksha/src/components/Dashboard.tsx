e9d1e141254ca3474bbb41147f3f7bef839e1364
Your Name <Hariharanath247@gmail.com>
2025-01-29 23:56:00 +0530

    hari

--- anna-raksha/src/components/Dashboard.tsx
+++ anna-raksha/src/components/Dashboard.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
-import { FiArrowRight, FiLogOut, FiCheckCircle, FaSignInAlt,} from 'react-icons/fi';
+import { FiArrowRight, FiLogOut, FiCheckCircle,} from 'react-icons/fi';
 import { FaSignInAlt } from 'react-icons/fa';
 
 const Dashboard: React.FC = () => {
